33012f59160f9c66aad2adbd2e0aadf975a76995
shubam rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-02-23 08:32:19 +0530

    id

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Image_Cdn } from '../utils/constant'
 
-function MovieCart({posterPath}) {
+function MovieCart({posterPath,id}) {
   return (
     <div className='flex w-40 items-center justify-center transition duration-300 hover:cursor-pointer  hover:scale-105'>
     <img 
+      onClick={() => console.log(id)}
       className='px-2 py-1 transition duration-300 transform hover:scale-105'
       src={Image_Cdn + posterPath}
       alt="Movie Cart" 
