4626b0929600868871728d69333d0c4b40708c5a
shubam rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-05-01 19:13:43 +0530

    vid

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import './App.css'
-import Body from './components/Body'
-import {Provider} from 'react-redux'
-import store from './store/store'
+import "./App.css";
+import Body from "./components/Body";
+import { Provider } from "react-redux";
+import store from "./store/store";
 import toast, { Toaster } from "react-hot-toast";
 
 function App() {
-
   return (
     <Provider store={store}>
-          <Body/>  
-          <Toaster
-  position="bottom-center"
-  reverseOrder={false}
-/>    
+      <Body />
+      <Toaster position="bottom-center" reverseOrder={false} />
     </Provider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -25,8 +25,6 @@ const MainContainer = () => {
 
   const { original_title, overview, id } = mainMovie;
 
-  
-
   return (
     <div className="bg-black md:pt-0">
       <VideoTitle title={original_title} overview={overview} />
